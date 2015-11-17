11e78443375f2aad69978608a09b19e2ae346d82
Tim Luo <tim@getclasskick.com>
2015-11-16 19:14:33 -0600

    Create a UITextField that shows the proposals' descriptions

--- DonorsChoose/ViewController.swift
+++ DonorsChoose/ViewController.swift
@@ -10,15 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // This outlet allows us to access the text view
+    @IBOutlet weak var proposalTextView: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // If there are search results, iterate over the returned proposals. For each one, print something out.
         if let resultsJSON = RestApiManager.sharedInstance.donorsChooseSearch(maxNumResults: 3, keywords: "Chicago") {
             // The iterator over a JSON always returns an (index, subJSON), which is why we need the empty first tuple value
+            var proposalsString = ""
             for (_, proposalJSON):(String, JSON) in resultsJSON["proposals"] {
                 let proposal = Proposal(json: proposalJSON)
-                print(proposal)
+                proposalsString += proposal.description + "\n"
             }
+            self.proposalTextView.text = proposalsString
         }
     }
 
