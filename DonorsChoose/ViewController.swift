a078facd5b26721bf67249f734881ef4534ea43c
Tim Luo <tim@getclasskick.com>
2015-11-16 19:38:33 -0600

    Created table view to show proposals, but it doesn't do much yet.

--- DonorsChoose/ViewController.swift
+++ DonorsChoose/ViewController.swift
@@ -8,24 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
-    // This outlet allows us to access the text view
-    @IBOutlet weak var proposalTextView: UITextView!
+    @IBOutlet weak var proposalTableView: UITableView!
+    var proposals: [Proposal] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // If there are search results, iterate over the returned proposals. For each one, print something out.
         if let resultsJSON = RestApiManager.sharedInstance.donorsChooseSearch(maxNumResults: 3, keywords: "Chicago") {
             // The iterator over a JSON always returns an (index, subJSON), which is why we need the empty first tuple value
-            var proposalsString = ""
             for (_, proposalJSON):(String, JSON) in resultsJSON["proposals"] {
                 let proposal = Proposal(json: proposalJSON)
-                proposalsString += proposal.description + "\n"
+                self.proposals.append(proposal)
             }
-            self.proposalTextView.text = proposalsString
         }
     }
 
+    // This is optional and defaults to 1 anyway, so we don't really need this.
+    // But we've added it here to show how to set the number of sections, how to reference
+    // a table view connected as an outlet, and being explicit about the number of sections.
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        if tableView == self.proposalTableView {
+            return 1
+        }
+        return 0
+    }
+
+    // UITableViewDataSource method defining the number of rows in the section
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.proposals.count
+    }
+
+    // UITableViewDataSource method specifying what the cell should look like for each row
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var proposalCell = tableView.dequeueReusableCellWithIdentifier("proposalCell")
+        if proposalCell == nil {
+            proposalCell = UITableViewCell()
+        }
+        proposalCell?.textLabel?.text = self.proposals[indexPath.row].description
+        return proposalCell!
+    }
 }
 
